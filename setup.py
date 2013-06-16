07b8c563121c6cc3273d9e1e2ed66c5206952b42
mattkoskela <mattkoskela@gmail.com>
2013-06-16 11:54:58 -0700

    Initial commit with namespace.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name="pybase",
+    version="1.0.0",
+    author="Matt Koskela",
+    author_email="mattkoskela@gmail.com",
+    packages=["namespace", "namespace.package_name"],
+    namespace_packages = ["namespace"],
+    url="http://example.com/",
+    license="LICENSE",
+    description="A quick starting point for namespaced python packages",
+    long_description=open("README.md").read(),
+    install_requires=[
+        "pymysql==0.5",
+    ]
+)
