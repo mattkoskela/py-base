0b1fd9a01ee9ba34f764ed5166f2b63ada87c550
Matt Koskela <mattkoskela@gmail.com>
2017-01-02 22:02:42 -0800

    Added script section and moved to setuptools

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="pybase",
-    version="1.0.0",
+    version="2.0.0",
     author="Matt Koskela",
     author_email="mattkoskela@gmail.com",
     packages=["namespace", "namespace.pybase"],
     namespace_packages = ["namespace"],
+    scripts=[
+        "bin/sample_script"
+    ],
     url="http://example.com/",
     license="LICENSE",
     description="A quick starting point for namespaced python packages",
     long_description=open("README.md").read(),
     install_requires=[
-        "pymysql==0.5",
+        "PyMySQL==0.7.9",
     ]
 )
